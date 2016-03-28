cd16a3b14473be7340c80cfd6c933651ffdfeee8
lam <ngoclamvt123@gmail.com>
2016-03-28 16:58:33 +0700

    Associate listing with users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160328064252) do
+ActiveRecord::Schema.define(version: 20160328085038) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "name",               limit: 255
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20160328064252) do
     t.string   "image_content_type", limit: 255
     t.integer  "image_file_size",    limit: 4
     t.datetime "image_updated_at"
+    t.integer  "user_id",            limit: 4
   end
 
   create_table "users", force: :cascade do |t|
